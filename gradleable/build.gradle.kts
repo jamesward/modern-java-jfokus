24aa4e5cefd80cecb5db1c0fd5ecf0e98c5dc8b0
James Ward <james@jamesward.com>
2022-06-15 08:05:25 -0600

    hum

--- gradleable/build.gradle.kts
+++ gradleable/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.6.21"
+    scala
 }
 
 java {
@@ -17,6 +18,14 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("net.bytebuddy:byte-buddy:1.10.8")
     implementation("net.bytebuddy:byte-buddy-agent:1.10.8")
+    implementation("org.scala-lang:scala3-library_3:3.1.2")
+
+    implementation("dev.zio:zio_3:2.0.0-RC5")
+    testImplementation("dev.zio:zio-test_3:2.0.0-RC5")
+
+    implementation("dev.optics:monocle-core_3:3.1.0")
+    implementation("dev.optics:monocle-unsafe_3:3.1.0")
+    implementation("dev.optics:monocle-macro_3:3.1.0")
 }
 
 tasks.withType<JavaCompile> {
@@ -29,13 +38,13 @@ tasks.register<JavaExec>("HelloRecords") {
     })
     dependsOn("classes")
     classpath = sourceSets["main"].runtimeClasspath
-    mainClass.set("HelloRecords")
+    mainClass.set("adt.HelloRecords")
 }
 
 tasks.register<JavaExec>("HelloSum") {
     dependsOn("classes")
     classpath = sourceSets["main"].runtimeClasspath
-    mainClass.set("HelloSum")
+    mainClass.set("adt.HelloSum")
     javaLauncher.set(javaToolchains.launcherFor {
         languageVersion.set(JavaLanguageVersion.of(17))
     })
@@ -48,5 +57,5 @@ tasks.register<JavaExec>("HelloNullHelp") {
     })
     dependsOn("classes")
     classpath = sourceSets["main"].runtimeClasspath
-    mainClass.set("HelloNullHelp")
+    mainClass.set("nullability.HelloNullHelp")
 }
